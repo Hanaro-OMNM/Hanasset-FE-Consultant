6f617432679553c18a29d4f61156111047783383
kkx7787 <kkx7787@naver.com>
2024-12-25 21:46:11 +0900

    feat: 임시 로그아웃 기능 구현 / 로그인 상태에 따른 접근 제어

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import { RecoilRoot, useRecoilState } from 'recoil';
 import { useEffect } from 'react';
 import './App.css';
@@ -10,17 +11,31 @@ import Consultant from './pages/consultant/Consultant.tsx';
 import isLoginAtom from './recoil/isLogin';
 
 function AppContent() {
-  const location = useLocation();
   const navigate = useNavigate();
-  const [isLogin] = useRecoilState(isLoginAtom);
+  const [isLogin, setIsLogin] = useRecoilState(isLoginAtom);
+
+  const isTokenExpired = (token: string) => {
+    try {
+      const decoded = jwtDecode(token); // 토큰 디코딩
+      const currentTime = Math.floor(Date.now() / 1000);
+      return decoded.exp! < currentTime; // 만료 여부 확인
+    } catch (error) {
+      console.error('Invalid token', error);
+      return true;
+    }
+  };
 
   useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (!accessToken || (accessToken && isTokenExpired(accessToken))) {
+      setIsLogin(false);
+    }
     if (!isLogin) {
       navigate('/login');
     } else {
       navigate('/');
     }
-  }, [isLogin, location.pathname, navigate]);
+  }, [isLogin]);
 
   return (
     <div className="App">
